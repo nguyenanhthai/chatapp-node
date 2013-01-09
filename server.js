be820457ede4f3ebe70091f1c3482c0c37fbdd16
Nguyen Anh Thai <thainguyen.0102@gmail.com>
2013-01-09 20:09:23 +0700

    fix port for heroku

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ var app = require('http').createServer(handler)
 , fs = require('fs')
 , io = require('socket.io').listen(app);
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+app.listen(5000);
 
 function handler(req, res) {
   console.log(__dirname);
